229a8430d22a0c957e0621092cb690cdf3ffb605
cascudo <lucas.sisau@gmail.com>
2019-08-12 11:25:06 -0300

    correct install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-docker exec -t laraschool_workspace_1 php artisan down
 docker exec -t laraschool_workspace_1 yarn
 docker exec -t laraschool_workspace_1 yarn run dev
 docker exec -t laraschool_workspace_1 composer install
